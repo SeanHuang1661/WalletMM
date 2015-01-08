6008b418785b6ed951a3ef9703e8e6b23cefec67
SeanHuang1661 <seanhuang1661@gmail.com>
2015-01-08 15:10:30 +0800

    15/1/8 15:00

--- app/src/main/java/com/sean/activity/ActivityMain.java
+++ app/src/main/java/com/sean/activity/ActivityMain.java
@@ -1,6 +1,7 @@
 package com.sean.activity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 import com.sean.walletmm2.R;
 import com.slidingmenu.lib.SlidingMenu;
@@ -11,7 +12,9 @@ public class ActivityMain extends ActivityFrame {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,R.layout.title_main);
         initView();
         initListener();
         initSlidingMenu();
