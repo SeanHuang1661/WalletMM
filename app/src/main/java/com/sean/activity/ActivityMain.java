ca1e074f7a09de9eca5f1a44a50e23d41bd9fb5e
SeanHuang1661 <seanhuang1661@gmail.com>
2015-01-08 20:00:14 +0800

    15/1/8 20:00

--- app/src/main/java/com/sean/activity/ActivityMain.java
+++ app/src/main/java/com/sean/activity/ActivityMain.java
@@ -1,10 +1,12 @@
 package com.sean.activity;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.sean.business.BusinessSetActionBar;
+import com.sean.fragment.FragmentSlidingmenu;
 import com.sean.walletmm2.R;
 import com.slidingmenu.lib.SlidingMenu;
 
@@ -15,6 +17,9 @@ public class ActivityMain extends ActivityFrame {
     Button titleMenu ;
 
     View actionBarView;
+
+    FragmentTransaction fragmentTransaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -38,6 +43,10 @@ public class ActivityMain extends ActivityFrame {
         menu.setFadeDegree(0.35f);
         menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
         menu.setMenu(R.layout.layout_slidingmenu);
+
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.laySlidingmenuContainer, FragmentSlidingmenu.getInstance());
+        fragmentTransaction.commit();
     }
 
     @Override
--- app/src/main/java/com/sean/fragment/FragmentSlidingmenu.java
+++ app/src/main/java/com/sean/fragment/FragmentSlidingmenu.java
@@ -10,17 +10,32 @@ import com.sean.walletmm2.R;
 
 public class FragmentSlidingmenu extends Fragment {
 
+    View slidingmenuView;
+
+    private static FragmentSlidingmenu fragmentSlidingmenu;
+
+    private FragmentSlidingmenu () {}
+
+    public static FragmentSlidingmenu getInstance () {
+        if (fragmentSlidingmenu == null) {
+            fragmentSlidingmenu = new FragmentSlidingmenu();
+        }
+        return fragmentSlidingmenu;
+    }
+
+    private void initAdapter () {
+        
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_slidingmenu, container, false);
+        slidingmenuView = inflater.inflate(R.layout.fragment_slidingmenu , container , false);
+        return slidingmenuView;
     }
 
 
